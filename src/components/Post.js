8e941ccaf0049ac10af5d38c9b2beefb2fe124d3
rafaelanobre <raafanobre@gmail.com>
2023-05-07 18:33:01 -0300

    dados dinamicos prontos, data test adicionadas

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,8 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Post(props) {
+    const [save, setSave] = useState('bookmark-outline');
+    const [like, setLike] = useState('heart-outline');
+    const [heartColor, setHeartColor] = useState('black');
+    const [likesCount, setLikesCount] = useState(Number(props.likesCount));
+
+    function savePost(){
+        setSave(save === 'bookmark' ? 'bookmark-outline' : 'bookmark');
+    }
+
+    function likePost() {
+        const newLike = like === 'heart' ? 'heart-outline' : 'heart';
+        const newHeartColor = like === 'heart' ? 'black' : 'red';
+        setLike(newLike);
+        setHeartColor(newHeartColor);
+
+        setLikesCount(prevCount => {
+            if (newLike === 'heart') {
+                return prevCount + 0.001;
+            } else {
+                return prevCount - 0.001;
+            }
+        });
+    }
+
+
     return (
-        <div className="post">
+        <div className="post" data-test="post">
             <div className="topo">
                 <div className="usuario">
                     <img src={props.avatar} alt={props.username} />
@@ -12,28 +37,27 @@ export default function Post(props) {
                     <ion-icon name="ellipsis-horizontal"></ion-icon>
                 </div>
             </div>
-
+    
             <div className="conteudo">
-                <img src={props.image} alt={props.alt} />
+                <img src={props.image} alt={props.alt} onDoubleClick={likePost} data-test="post-image" />
             </div>
-
+    
             <div className="fundo">
                 <div className="acoes">
                     <div>
-                        <ion-icon name="heart-outline"></ion-icon>
+                        <ion-icon onClick={likePost} name={like} data-test="like-post" style={{ color: heartColor }}></ion-icon>
                         <ion-icon name="chatbubble-outline"></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
+                        <ion-icon name={save} onClick={savePost} data-test="save-post"></ion-icon>
                     </div>
                 </div>
-
+    
                 <div className="curtidas">
                     <img src={props.likesAvatar} alt={props.likesUsername} />
-                    <div className="texto">
-                        Curtido por <strong>{props.likesUsername}</strong> e{" "}
-                        <strong>outras {props.likesCount} pessoas</strong>
+                    <div className="texto" data-test="likes-number">
+                        Curtido por <strong>{props.likesUsername}</strong> e <strong>outras {likesCount} pessoas</strong>
                     </div>
                 </div>
             </div>
--- src/components/User.js
+++ src/components/User.js
@@ -2,18 +2,33 @@ import { useState } from "react";
 
 export default function User() {
     const [myUser, setMyUser] = useState('catanacomics');
+    const [myAvatar, setMyAvatar] = useState('assets/img/catanacomics.svg');
 
     function changeUser(){
-        prompt('Quer alterar seu nome de usuário? Digite o novo nome')
+        let newUser = prompt('Quer alterar seu nome de usuário? Digite o novo nome');
+        if (newUser && newUser.trim() !== '') {
+            setMyUser(newUser.trim());
+        } else{
+            alert('Vamos manter o nome de usuário então!')
+        }
+    }
+
+    function changeAvatar(){
+        let newAvatar = prompt('Quer alterar sua imagem? Digite o link para a nova imagem');
+        if (newAvatar && newAvatar.trim() !== '') {
+            setMyAvatar(newAvatar.trim());
+        } else{
+            alert('Vamos manter a imagem então!')
+        }
     }
 
     return (
         <div className="usuario">
-            <img src="assets/img/catanacomics.svg" alt="imagem de perfil" />
-            <div className="texto">
+            <img src={myAvatar} onClick={changeAvatar} alt="imagem de perfil" data-test="profile-image"/>
+            <div className="texto" data-test="name">
                 <span>
                     <strong>{myUser}</strong>
-                    <ion-icon name="pencil" onClick={changeUser}></ion-icon>
+                    <ion-icon name="pencil" onClick={changeUser} data-test="edit-name"></ion-icon>
                 </span>
             </div>
         </div>
